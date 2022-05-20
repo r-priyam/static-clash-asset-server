c1bfeb33b4468aadced82956f9d24cb1eb5d5e2b
Priyam <50884372+r-priyam@users.noreply.github.com>
2022-05-20 15:32:54 +0530

    fix: `/` route is actually `/api`

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const app = express()
 
 app.use(express.static('/public'))
 
-app.get('/', (_req, res) => {
+app.get('/api', (_req, res) => {
     res.send(`This content is not affiliated with, endorsed, sponsored, or specifically approved by Supercell and Supercell is not responsible for it.
     For more information see https://supercell.com/en/fan-content-policy/`)
 })
