fb7827df7bb79d5bb1b11fb098a8670f539b4fe2
Priyam <50884372+r-priyam@users.noreply.github.com>
2022-05-20 13:35:09 +0530

    feat: initial commit

---
+++ index.mjs
@@ -0,0 +1,16 @@
+import Fastify from 'fastify';
+import { fileURLToPath } from 'node:url';
+import fastifyStatic from '@fastify/static';
+
+
+const fastify = Fastify({ logger: false })
+
+fastify.register(fastifyStatic, {
+    root: fileURLToPath(new URL('public', import.meta.url)),
+    prefix: '/static/',
+})
+
+fastify.listen({ port: 3000 }, (err, address) => {
+    if (err) throw err
+    console.log(`Server listening on ${address}`)
+})
