9b3840b86055de7b745e936f206d031db949a406
Priyam <50884372+r-priyam@users.noreply.github.com>
2022-05-20 15:31:18 +0530

    chore: quick chanegs
    It works, yessssss

--- api/index.js
+++ api/index.js
@@ -4,4 +4,9 @@ const app = express()
 
 app.use(express.static('/public'))
 
+app.get('/', (_req, res) => {
+    res.send(`This content is not affiliated with, endorsed, sponsored, or specifically approved by Supercell and Supercell is not responsible for it.
+    For more information see https://supercell.com/en/fan-content-policy/`)
+})
+
 module.exports = app;
